1ba4184a0b8ed7f487808ceaf1c1b971c2eb7b07
Qwexaro <Sergikstar25.gold@mail.ru>
2025-02-16 21:52:51 +0300

    Revert "Updated"
    This reverts commit 2b06fdfb469b972c59eb12355cc980769e21bea9.

--- The_Pointer_Game/Quests.cpp
+++ The_Pointer_Game/Quests.cpp
@@ -4,15 +4,15 @@ void Menu::questSuccesful(std::vector<std::string>& menu_items, std::vector<std:
 {
     switch (getTotalScore())
     {
-        case 0: setTotalScore(1); menu_items.push_back("РџРѕРјРµРЅСЏС‚СЊ С†РІРµС‚ РјРµРЅСЋ"); break;
-        case 1: setTotalScore(1); menu_items.push_back("РљСѓСЂСЃРѕСЂС‹"); color_options.push_back("РџСѓСЂРїСѓСЂРЅС‹Р№"); break;
+        case 0: setTotalScore(1); menu_items.push_back("Поменять цвет меню"); break;
+        case 1: setTotalScore(1); menu_items.push_back("Курсоры"); color_options.push_back("Пурпурный"); break;
         case 2: setTotalScore(1); cursor_options.push_back("~$"); break;
-        case 3: setTotalScore(1); cursor_options.push_back(":_>"); color_options.push_back("Р‘РёСЂСЋР·РѕРІС‹Р№"); break;
+        case 3: setTotalScore(1); cursor_options.push_back(":_>"); color_options.push_back("Бирюзовый"); break;
         case 4: setTotalScore(1); cursor_options.push_back("+");  break;
-        case 5: setTotalScore(1); cursor_options.push_back("~"); color_options.push_back("РЎРІРµС‚Р»Рѕ-СЃРёРЅРёР№"); menu_items.push_back("РњР°РіР°Р·РёРЅ"); break;
+        case 5: setTotalScore(1); cursor_options.push_back("~"); color_options.push_back("Светло-синий"); menu_items.push_back("Магазин"); break;
         case 6: setTotalScore(1); cursor_options.push_back("~@"); break;
-        case 7: setTotalScore(1); cursor_options.push_back("~%"); color_options.push_back("РЎРёРЅРёР№"); break;
-        case 8: setTotalScore(1); cursor_options.push_back("~#"); color_options.push_back("Р–С‘Р»С‚С‹Р№"); break;
+        case 7: setTotalScore(1); cursor_options.push_back("~%"); color_options.push_back("Синий"); break;
+        case 8: setTotalScore(1); cursor_options.push_back("~#"); color_options.push_back("Жёлтый"); break;
         default: break;
     }
 }
@@ -22,16 +22,16 @@ void Menu::quests()
     system("cls");
     switch (getTotalScore())
     {
-        case 0: std::cout << "РљРІРµСЃС‚ (РѕС‚РєСЂС‹С‚СЊ РјРµРЅСЋ СЃ РєРІРµСЃС‚Р°РјРё) РІС‹РїРѕР»РЅРµРЅ!" << std::endl; break;
-        case 1: std::cout << "РїРѕРјРµРЅСЏР№С‚Рµ РёРЅРІРµСЂСЃРёСЋ" << std::endl; break;
-        case 2: std::cout << "РќР°Р¶РјРёС‚Рµ РЅР° РєРЅРѕРїРєСѓ 'Р·РµР»РµРЅС‹Р№'" << std::endl; break;
-        case 3: std::cout << "РџРѕРјРµРЅСЏР№С‚Рµ РєСѓСЂСЃРѕСЂ РЅР° ~$" << std::endl; break;
-        case 4: std::cout << "РќР°Р¶РјРёС‚Рµ РЅР° РєРЅРѕРїРєСѓ 'РїСѓСЂРїСѓСЂРЅС‹Р№'" << std::endl; break;
-        case 5: std::cout << "Р’С‹РєР»СЋС‡РёС‚Рµ РёРЅРІРµСЂСЃРёСЋ Рё РІС‹Р±РµСЂРёС‚Рµ РєСѓСЂСЃРѕСЂ '+'" << std::endl; break;
-        case 6: std::cout << "РљСѓРїРёС‚Рµ РЅРµРѕР±С‹С‡РЅС‹Р№ СѓРєР°Р·Р°С‚РµР»СЊ: '-<x<o>>'" << std::endl; break;
-        case 7: std::cout << "Р’С‹Р±РµСЂРёС‚Рµ СѓРєР°Р·Р°С‚РµР»СЊ '~>', РІРєР»СЋС‡РёС‚Рµ РёРЅРІРµСЂСЃРёСЋ Рё РІ РєРѕРЅС†Рµ РІС‹Р±РµСЂРёС‚Рµ 'Р±РёСЂСЋР·РѕРІС‹Р№'" << std::endl; break;
-        case 8: std::cout << "РќР°Р±РµСЂРёС‚Рµ 100 points РІ РјР°РіР°Р·РёРЅРµ" << std::endl; break;
-        default: std::cout << "Р’С‹ РІС‹РїРѕР»РЅРёР»Рё РІСЃРµ РєРІРµСЃС‚С‹!" << std::endl; break;
+        case 0: std::cout << "Квест (открыть меню с квестами) выполнен!" << std::endl; break;
+        case 1: std::cout << "поменяйте инверсию" << std::endl; break;
+        case 2: std::cout << "Нажмите на кнопку 'зеленый'" << std::endl; break;
+        case 3: std::cout << "Поменяйте курсор на ~$" << std::endl; break;
+        case 4: std::cout << "Нажмите на кнопку 'пурпурный'" << std::endl; break;
+        case 5: std::cout << "Выключите инверсию и выберите курсор '+'" << std::endl; break;
+        case 6: std::cout << "Купите необычный указатель: '-<x<o>>'" << std::endl; break;
+        case 7: std::cout << "Выберите указатель '~>', включите инверсию и в конце выберите 'бирюзовый'" << std::endl; break;
+        case 8: std::cout << "Наберите 100 points в магазине" << std::endl; break;
+        default: std::cout << "Вы выполнили все квесты!" << std::endl; break;
     }
 
     if (getTotalScore() == 0) questSuccesful(menu_items, color_options, cursor_options);
